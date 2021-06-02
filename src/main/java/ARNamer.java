b269b1b06af381ebc80d40eb9fbe2376f8cff39f
inniHans <crazyun2005@naver.com>
2021-06-02 15:26:31 +0900

    regex

--- src/main/java/ARNamer.java
+++ src/main/java/ARNamer.java
@@ -1,30 +1,31 @@
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-@Slf4j
+//@Slf4j
 public class ARNamer {
-    static int NOT_NUM = -9999;
-    static int DATE_DIGITS = 6;
-    static int DATE_SEPARATE = 2;
     static String HYPHEN = "-";
     static String DOT = ".";
+    static String SLASH = "/";
+    static String FORMAT = "m4a";
+    static String DOT_HYPHEN_REGEX = "[-.]";
+    static String DATE_REGEX = "(19|20)\\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(0[0-9]|1[0-9]|2[0-4])([0-5][0-9])([0-5][0-9])";
+
 
     public static void main(String[] args) throws IOException {
-        //        renaming();
         String path = System.getProperty("path");
         changeFiles(path);
     }
 
     static void changeFiles(String path) throws IOException {
-        int count = 0;
-        int folders = 0;
-        int error = 0;
+        int[] inf = {0, 0, 0};
 
         File dir = new File(path);
 
@@ -32,113 +33,67 @@ public class ARNamer {
             File[] files = dir.listFiles();
 
             if (files == null) {
-                System.out.println("Null File list");
+                System.out.println("!!!Null File list");
                 return;
             }
 
-            System.out.println("[START] " + files.length + " files");
+            System.out.println("[START] " + files.length + " items");
 
             for (File file : files) {
                 if (!Files.isDirectory(file.toPath())) {
                     String resultMsg;
                     String oldName = file.getName();
-                    String newName = changeName(file.getName());
+                    String newName = changeName(file.getName(), path);
 
                     resultMsg = (newName == null)
                             ? "Error(1) - " + oldName
-                            : file.renameTo(new File(dirConfig(newName, path)))
-                            ? "Success - " + oldName + " -------> " + newName
-                            : "Error(2) - " + oldName;
+                            : file.renameTo(new File(newName))
+                                ? "Success - " + oldName + " -------> " + newName
+                                : "Error(2) - " + oldName;
 
-                    if (resultMsg.contains("Success")) {
-                        count++;
-                    } else {
-                        error++;
-                    }
-
+                    inf[resultMsg.contains("Success") ? 0 : 1]++;
                     System.out.println(resultMsg);
                 } else {
-                    folders++;
+                    inf[2]++;
                 }
             }
-            System.out.println("[END] " + count + " successes / " + error + " errors / " + folders + " folders");
+            System.out.println("[END] " + inf[0] + " successes / " + inf[1] + " skips / " + inf[2] + " folders");
         } else {
-            System.out.println("Not a directory");
+            System.out.println("!!!Not a directory");
         }
     }
 
-    static String changeName(String oldName) {
-        String[] nameAndFormat = oldName.split(Pattern.quote(DOT));
+    static String changeName(String oldName, String path) throws IOException {
+        String[] splits = oldName.split(DOT_HYPHEN_REGEX);
+        int finalIndex = splits.length - 1;
 
-        if (nameAndFormat.length != 2)
+        if (finalIndex < 9 || !splits[finalIndex].equals(FORMAT) || Pattern.matches(DATE_REGEX, Arrays.asList(splits).subList(finalIndex - 6, finalIndex).toString()))
             return null;
 
-        String[] parts = nameAndFormat[0].split(Pattern.quote(HYPHEN));
-        int num = parts.length;
-
-        if (num < 8 || isNumeric(parts[0]) != NOT_NUM)
-            return null;
-
-        int dateStart = num - DATE_DIGITS;
+        Collections.rotate(Arrays.asList(splits).subList(0, finalIndex), 6);
 
         StringBuilder result = new StringBuilder();
+        AtomicInteger count = new AtomicInteger();
 
-        for (int i = dateStart; i < num; i++){
-            result.append(parts[i]);
-            if (i == dateStart + DATE_SEPARATE || i == num -1)
+        result.append(path).append(SLASH)
+                .append(splits[0]).append(SLASH)
+                .append(splits[1]);
+
+        Path newPath = Paths.get(result.toString());
+        if (!Files.isDirectory(newPath))
+            Files.createDirectories(newPath);
+
+        result.append(SLASH);
+
+        Stream.of(splits).forEach(next -> {
+            result.append(next);
+            int index = count.getAndIncrement();
+            if (index == (finalIndex - 1)) {
+                result.append(DOT);
+            } else if (index == 2 || (index >= 5 && index < finalIndex -1)) {
                 result.append(HYPHEN);
-        }
-
-        for (int j = 0; j < dateStart; j++){
-            result.append(parts[j]);
-            if (j != dateStart -1)
-                result.append(HYPHEN);
-        }
-
-        result.append(DOT).append(nameAndFormat[1]);
+            }
+        });
         return result.toString();
     }
-
-    static String dirConfig(String name, String path) throws IOException {
-        int year = isNumeric(name.substring(0, 4));
-        int month = isNumeric(name.substring(4,6));
-
-        if (year > 2200 || year < 2000 || month < 0 || month > 12)
-            return path + "/" + name;
-
-        String dir = path + "/" + year + "/" + month;
-
-        Path dstPath = Paths.get(dir);
-        if (!Files.isDirectory(dstPath))
-            Files.createDirectories(dstPath);
-
-        return dstPath + "/" + name;
-    }
-
-    static int isNumeric(String string) {
-        if(string == null || string.equals(""))
-            return NOT_NUM;
-
-        try {
-            return Integer.parseInt(string);
-        } catch (NumberFormatException e) {
-            return NOT_NUM;
-        }
-    }
-
-
-//    static void renaming() {
-//        File dir = new File(pathInput);
-//        if(dir.exists()) {
-//            File[] files = dir.listFiles();
-//            for (File file : files) {
-//                if (!Files.isDirectory(file.toPath())){
-//                    String fileName = file.getName();
-//                    if(fileName.contains("FaceTime") || fileName.contains("TelephonyCall")){
-//                        file.renameTo(new File(pathInput + "/" + fileName.replace("FaceTime", "FT").replace("TelephonyCall", "Tel")));
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
